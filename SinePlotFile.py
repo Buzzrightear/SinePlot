88d57b6b41957e9d087010f5f8c04c2d9313da8c
Buzzrightear <mail@henrystanton.co.uk>
2020-06-16 17:43:57 +0100

    Sine wave plotting experimental
    Sine wave plotting experimental

--- SinePlotFile.py
+++ SinePlotFile.py
@@ -1,16 +1,26 @@
 # Author: Henry Stanton
 # Date created: 16/06/2020
 import math
-import matplotlib .pyplot as plt
+import matplotlib.pyplot as plt
 
 x = 0.1
-valuesList = []
+y = 0.9
+
+valuesListX = []
+valuesListY = []
 
 # Create array of values for sine wave
 for i in range(200):
-    valuesList.append(math.sin(x))
+    valuesListX.append(math.sin(x))
+    valuesListY.append(math.sin(y))
     x += 0.1
+    y += 0.1
 
 # plot values
-
+plt.plot(valuesListX, color = 'red', marker = "o")
+plt.plot(valuesListY, color = 'green', marker = "x")
+plt.title("Some values of sine")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
 
