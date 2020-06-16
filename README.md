d5bbb858727b3f47eddec835b3f3b154885315c2
Buzzrightear <mail@henrystanton.co.uk>
2020-06-16 17:21:04 +0100

    Outline defined. Readme added.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 #SinePlot
-##Attempt to plot a sine wave using a previously generated dataset
-##N.B. Will probably change this to one generated on the fly
+##Attempt to plot a sine wave using a dataset generated on the fly
 
 >Mostly an exercise in re-learning python/pycharm/github
--- SinePlotFile.py
+++ SinePlotFile.py
@@ -1,5 +1,16 @@
 # Author: Henry Stanton
 # Date created: 16/06/2020
+import math
+import matplotlib .pyplot as plt
 
-print("hello world")
+x = 0.1
+valuesList = []
+
+# Create array of values for sine wave
+for i in range(200):
+    valuesList.append(math.sin(x))
+    x += 0.1
+
+# plot values
+
 
